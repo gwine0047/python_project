8f1a745935e370792e297544eabd3d7e22e42fcb
Okoeguale Godwin <okoegualegodwin@gamil.com>
2023-11-18 18:34:32 +0100

    made a snake game

---
+++ snake-game/game.py
@@ -0,0 +1,152 @@
+import turtle
+import time
+import random
+
+# Set up the screen
+screen = turtle.Screen()
+screen.title("Snake Game")
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.tracer(0)  # Turns off automatic screen updates
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "Right"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+# Snake body
+segments = []
+
+# Score
+score = 0
+
+# Functions
+def go_up():
+    if head.direction != "Down":
+        head.direction = "Up"
+
+def go_down():
+    if head.direction != "Up":
+        head.direction = "Down"
+
+def go_left():
+    if head.direction != "Right":
+        head.direction = "Left"
+
+def go_right():
+    if head.direction != "Left":
+        head.direction = "Right"
+
+def move():
+    if head.direction == "Up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    elif head.direction == "Down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    elif head.direction == "Left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    elif head.direction == "Right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+screen.onkey(go_left, "Left")
+screen.onkey(go_right, "Right")
+
+# Main game loop
+while True:
+    screen.update()
+
+    # Check for collision with border
+    if (
+        head.xcor() > 290
+        or head.xcor() < -290
+        or head.ycor() > 290
+        or head.ycor() < -290
+    ):
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "Right"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # Clear the segments list
+        segments.clear()
+
+        # Reset the score
+        score = 0
+
+    # Check for collision with food
+    if head.distance(food) < 20:
+        # Move the food to a random position
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x, y)
+
+        # Add a segment to the snake
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Increase the score
+        score += 10
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    # Move the first segment to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    # Call the move function
+    move()
+
+    # Check for head collisions with body segments
+    for segment in segments:
+        if head.distance(segment) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "Right"
+
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            score = 0
+
+    # Update the score display
+    time.sleep(0.1)
